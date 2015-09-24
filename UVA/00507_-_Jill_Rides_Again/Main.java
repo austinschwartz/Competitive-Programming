293b760dd54cb1564fda9bed059568164ff285e1
nawns <nonis@protonmail.com>
2015-09-24 15:42:48 -0400

    507 fixed

--- UVA/00507_-_Jill_Rides_Again/Main.java
+++ UVA/00507_-_Jill_Rides_Again/Main.java
@@ -11,25 +11,25 @@ class Main {
       for (int j = 0; j < s - 1; j++) {
         stops[j] = Integer.parseInt(br.readLine().trim());
       }
-      int max_so_far = 0;
-      int max_ending_here = 0;
-      for (int k = 0; k < s - 1; k++) {
-        max_ending_here += stops[k];
-        if (max_ending_here < 0) {
-          max_ending_here = 0;
-          start = k;
+      int start = 0, startf = 0, end = -1;
+      int sum = 0, ans = 0;
+      for (int j = 0; j < s - 1; j++) {
+        sum += stops[j];
+        if (sum < 0) {
+          sum = 0;
+          start = j + 1;
         }
-        if (max_so_far < max_ending_here) {
-          max_so_far = max_ending_here;
-          end = k;
+        if (sum > ans || (sum == ans && j-start > end - start)) {
+          ans = sum;
+          end = j;
+          startf = start;
         }
       }
-      System.out.println(Arrays.toString(stops));
-      System.out.println(max_so_far);
-      if (max_so_far == 0)
+      if (ans == 0)
         System.out.println("Route " + (i + 1) + " has no nice parts");
       else
-        System.out.println("The nicest part of route " + (i+1) + " is between stops " + (start + 2) + " and " + (end + 2));
+        System.out.println("The nicest part of route " + (i + 1) + 
+            " is between stops " + (startf+1) + " and " + (end + 2));
     }
   }
 }
