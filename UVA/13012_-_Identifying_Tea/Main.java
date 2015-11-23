380534fc9a68505dedd1a39ff77aee559dffe94c
nawns <nonis@protonmail.com>
2015-11-22 23:45:37 -0500

    13012

---
+++ UVA/13012_-_Identifying_Tea/Main.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static PrintWriter out;
+  public static void main(String[] args) {
+    MyScanner sc = new MyScanner();
+    out = new PrintWriter(new BufferedOutputStream(System.out));
+
+    while (sc.hasNext()) {
+      int T = sc.nextInt();
+      int A = sc.nextInt();
+      int B = sc.nextInt();
+      int C = sc.nextInt();
+      int D = sc.nextInt();
+      int E = sc.nextInt();
+      int count = 0;
+      if (A == T) count++;
+      if (B == T) count++;
+      if (C == T) count++;
+      if (D == T) count++;
+      if (E == T) count++;
+      System.out.println(count);
+    }
+
+    out.close();
+  }
+
+
+  public static class MyScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public MyScanner() {
+       br = new BufferedReader(new InputStreamReader(System.in));
+    }
+ 
+    public boolean hasNext() {
+      try {
+        boolean a = br.ready();
+        return a;
+      } catch (IOException e) {
+        return false;
+      }
+    }
+
+    public String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+ 
+    public int nextInt() {
+      return Integer.parseInt(next());
+    }
+ 
+    public long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    public char nextChar() {
+      return next().charAt(0);
+    }
+
+    public String nextLine() {
+      String str = "";
+      try {
+         str = br.readLine();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
