9ec3e2a990f2a16fd44c0c2712becd1f92e6b841
nawns <nonis@protonmail.com>
2015-12-07 22:51:45 -0800

    TLE

---
+++ UVA/00195_-_Anagram-gaveupon/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+  public static List<String> permutations(String word, int n) {
+    List<String> list = new ArrayList<String>();
+    if (n == 0) {
+      list.add("");
+    } else {
+      List<String> prev = permutations(word, n - 1);
+      for (String str : prev) {
+        for (int i = 0; i <= str.length(); i++) {
+          list.add(str.substring(0, i) + word.charAt(n - 1) + str.substring(i));
+        }
+      }
+    }
+    return list;
+  }
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    for (int i = 0; i < n; i++) {
+      String word = br.readLine();
+      List<String> words = permutations(word, word.length());
+      Collections.sort(words);
+      for (String w : words)
+        System.out.println(w);
+    }
+  }
+}
