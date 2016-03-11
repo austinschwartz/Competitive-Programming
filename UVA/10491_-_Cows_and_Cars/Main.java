1140d893acfdc427f4f5bb6689907286c6105058
nawns <nonis@protonmail.com>
2016-03-10 19:12:51 -0500

    10491

---
+++ UVA/10491_-_Cows_and_Cars/Main.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static PrintWriter out;
+  public static void main(String[] args) {
+    MyScanner sc = new MyScanner();
+    out = new PrintWriter(new BufferedOutputStream(System.out));
+    while (sc.hasNext()) {
+      int cows = sc.nextInt();
+      int cars = sc.nextInt();
+      int show = sc.nextInt();
+      out.printf("%.5f\n", prob(cows, cars, show));
+    }
+    out.close();
+  }
+
+  public static double prob(int cows, int cars, int show) {
+    int doors = cows + cars;
+    int doors_to_pick_from = doors - 1;
+
+    return (cars * doors_to_pick_from) / 
+              (double)(doors * (doors_to_pick_from - show));
+  }
+
+
+  public static class MyScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public MyScanner() {
+       br = new BufferedReader(new InputStreamReader(System.in));
+    }
+ 
+    public boolean hasNext() {
+      try {
+        boolean a = br.ready();
+        return a;
+      } catch (IOException e) {
+        return false;
+      }
+    }
+
+    public String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+ 
+    public int nextInt() {
+      return Integer.parseInt(next());
+    }
+ 
+    public long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    public char nextChar() {
+      return next().charAt(0);
+    }
+
+    public String nextLine() {
+      String str = "";
+      try {
+         str = br.readLine();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
