df718f3b6b705a17173e0415c1a57dfbf86e3544
nonis3 <nonis@purdue.edu>
2017-03-16 09:31:05 -0400

    Removing some files

--- codeforces/problems/0217A_-_Ice_Skating/Main.java
+++ codeforces/problems/0217A_-_Ice_Skating/Main.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Main {
   public static PrintWriter out;
-  public static int[] parent;
   public static int max = 1111;
   public static int n;
   public static int[] x, y;
@@ -12,14 +11,11 @@ public class Main {
     MyScanner sc = new MyScanner();
     out = new PrintWriter(new BufferedOutputStream(System.out));
     n = sc.nextInt();
-    parent = new int[n];
-    Arrays.fill(parent, -1);
     x = new int[n];
     y = new int[n];
     for (int i = 0; i < n; i++) {
       int a = sc.nextInt();
       int b = sc.nextInt();
-      parent[i] = i;
       x[i] = a;
       y[i] = b;
     }
@@ -28,37 +24,21 @@ public class Main {
     for (int i = 0; i < n; i++) {
       if (visited[i])
         continue;
-      visited[i] = true;
-      for (int j = 0; j < n; j++) {
-        if (visited[j]) continue;
-        if (find(i) == find(j)) continue;
-        if (x[i] == x[j] || y[i] == y[j]) {
-          merge(i, j);
-          visited[j] = true;
-        }
-      }
+      dfs(i);
+      num += 1;
     }
-    Arrays.sort(parent);
-    for (int i = 1; i < n; i++) {
-      if (parent[i] != parent[i-1])
-        num++;
-    }
-    out.println(num);
+    out.println(num - 1);
     out.close();
   }
 
-  public static int find(int x) {
-    if (x != parent[x])
-      parent[x] = find(parent[x]);
-    return parent[x];
-  }
-
-  public static Random random = new Random();
-  public static void merge(int p, int q) {
-    p = find(p);
-    q = find(q);
-    if (random.nextBoolean()) parent[p] = q;
-    else parent[q] = p;
+  public static void dfs(int v) {
+    visited[v] = true;
+    for (int i = 0; i < n; i++) {
+      if (visited[i]) continue;
+      if (x[i] == x[v] || y[i] == y[v]) {
+        dfs(i);
+      }
+    }
   }
 
   public static boolean oob(int i, int j) {
