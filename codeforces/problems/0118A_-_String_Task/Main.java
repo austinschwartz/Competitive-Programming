558473f111b0a818cd35494fa618cb0aa78a9169
nonis3 <nonis@purdue.edu>
2017-03-18 21:19:00 -0400

    118A

---
+++ codeforces/problems/0118A_-_String_Task/Main.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static PrintWriter out;
+  public static void main(String[] args) {
+    MyScanner sc = new MyScanner();
+    out = new PrintWriter(new BufferedOutputStream(System.out));
+    StringBuilder sb = new StringBuilder();
+    for (char c : sc.nextLine().toCharArray())
+      if (!isVowel(c))
+        sb.append(Character.toLowerCase(c));
+    for (char c : sb.toString().toCharArray())
+      out.print("." + c);
+    out.println();
+    out.close();
+  }
+
+  public static boolean isVowel(char c) {
+    c = Character.toLowerCase(c);
+    if (c == 'y' || c == 'a' || c == 'e' ||
+        c == 'i' || c == 'o' || c == 'u')
+      return true;
+    return false;
+  }
+
+
+  public static class MyScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public MyScanner() {
+       br = new BufferedReader(new InputStreamReader(System.in));
+    }
+ 
+    public boolean hasNext() {
+      try {
+        boolean a = br.ready();
+        return a;
+      } catch (IOException e) {
+        return false;
+      }
+    }
+
+    public String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+ 
+    public int nextInt() {
+      return Integer.parseInt(next());
+    }
+ 
+    public long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    public char nextChar() {
+      return next().charAt(0);
+    }
+
+    public String nextLine() {
+      String str = "";
+      try {
+         str = br.readLine();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
