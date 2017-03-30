30abc3870b1afa4ce80420519fd39f2d7320bba8
nonis3 <nonis@purdue.edu>
2017-03-30 13:55:22 -0400

    349A

---
+++ codeforces/0349A_-_Cinema_Line/Main.java
@@ -0,0 +1,102 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static PrintWriter out;
+  public static void main(String[] args) {
+    MyScanner sc = new MyScanner();
+    out = new PrintWriter(new BufferedOutputStream(System.out));
+    int n = sc.nextInt();
+    int b25 = 0, b50 = 0, b100 = 0;
+    boolean flag = true;
+    int i = 0;
+    for (i = 0; i < n; i++) {
+      int x = sc.nextInt();
+      if (x == 25) {
+        b25 += 1;
+      } else if (x == 50) {
+        if (b25 == 0) {
+          flag = false;
+          break;
+        } else {
+          b50 += 1;
+          b25 -= 1;
+        }
+      } else if (x == 100) {
+        if (b50 >= 1 && b25 >= 1) {
+          b50 -= 1;
+          b25 -= 1;
+          b100 += 1;
+        } else if (b25 < 3) {
+          flag = false;
+          break;
+        } else {
+          b100 += 1;
+          b25 -= 3;
+        }
+      }
+    }
+
+    if (flag && i == n)
+      out.println("YES");
+    else
+      out.println("NO");
+    out.close();
+  }
+
+
+  public static class MyScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public MyScanner() {
+       br = new BufferedReader(new InputStreamReader(System.in));
+    }
+ 
+    public boolean hasNext() {
+      try {
+        boolean a = br.ready();
+        return a;
+      } catch (IOException e) {
+        return false;
+      }
+    }
+
+    public String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+ 
+    public int nextInt() {
+      return Integer.parseInt(next());
+    }
+ 
+    public long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    public char nextChar() {
+      return next().charAt(0);
+    }
+
+    public String nextLine() {
+      String str = "";
+      try {
+         str = br.readLine();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
