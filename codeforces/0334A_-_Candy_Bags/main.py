23f2e52976a8193092b66f38aa7264d69491cd61
nonis3 <nonis@purdue.edu>
2017-04-11 12:51:13 -0400

    334A

---
+++ codeforces/0334A_-_Candy_Bags/main.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+s = int((n*n)*(n*n + 1)/2)
+each_gets = s // n
+
+res = []
+
+used = set()
+
+cur = []
+for x in range(0, n*n):
+    i = n*n - x
+    j = each_gets//(n//2) - i
+    if j not in used and i not in used:
+        used.add(i)
+        used.add(j)
+        cur.append(i)
+        cur.append(j)
+        if len(cur) == n:
+            res.append(cur)
+            cur = []
+
+for thing in res:
+    print(' '.join(map(str,thing)))
