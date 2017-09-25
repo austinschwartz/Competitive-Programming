4473d93a85d239b3c113f24620f1ec37eda48ad9
nonis3 <nonis@purdue.edu>
2017-09-25 06:53:23 -0700

    11136

--- UVA/11136_-_Hoax_or_what/Main.java
+++ UVA/11136_-_Hoax_or_what/Main.java
@@ -1,46 +1,99 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Main {
-  public static void main(String[] args) throws Exception {
-    Scanner in = new Scanner(System.in);
-    while (in.hasNextInt()) {
-      int n = in.nextInt();
+public class Main {
+  public static PrintWriter out;
+  public static void main(String[] args) {
+    MyScanner sc = new MyScanner();
+    out = new PrintWriter(new BufferedOutputStream(System.out));
+    while (true) {
+      int n = sc.nextInt();
       if (n == 0)
         break;
-      int total = 0;
-      Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+      long total = 0;
+      TreeMap<Long, Long> map = new TreeMap<>();
       for (int i = 0; i < n; i++) {
-        int k = in.nextInt();
+        int k = sc.nextInt();
         for (int j = 0; j < k; j++) {
-          int x = in.nextInt();
+          long x = sc.nextLong();
           if (map.containsKey(x))
-            map.put(x, map.get(x) + 1);
+            map.put(x, map.get(x) + 1L);
           else
-            map.put(x, 1);
+            map.put(x, 1L);
         }
-        int low = Integer.MAX_VALUE;
-        int high = -1;
-        for (int s : map.keySet()) {
-          if (map.get(s) > 0) {
-            if (s < low)
-              low = s;
-            if (s > high)
-              high = s;
-          }
-        }
+ 
+        long low = map.firstKey();
+        long high = map.lastKey();
         total += (high - low);
-        if (map.containsKey(high))
-          map.put(high, map.get(high) - 1);
+
+        if (map.containsKey(high) && map.get(high) > 1L)
+          map.put(high, map.get(high) - 1L);
         else
           map.remove(high);
-        if (map.containsKey(low))
-          map.put(low, map.get(low) - 1);
+
+        if (map.containsKey(low) && map.get(low) > 1L)
+          map.put(low, map.get(low) - 1L);
         else
           map.remove(low);
-
       }
-      System.out.println(total);
+      out.println(total);
+    }
+    out.close();
+  }
+
+
+  public static class MyScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public MyScanner() {
+       br = new BufferedReader(new InputStreamReader(System.in));
+    }
+ 
+    public boolean hasNext() {
+      try {
+        boolean a = br.ready();
+        return a;
+      } catch (IOException e) {
+        return false;
+      }
+    }
+
+    public String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+ 
+    public int nextInt() {
+      return Integer.parseInt(next());
+    }
+ 
+    public long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    public char nextChar() {
+      return next().charAt(0);
+    }
+
+    public String nextLine() {
+      String str = "";
+      try {
+         str = br.readLine();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      return str;
     }
   }
 }
