0f7b389cdff5ebaf0ee21b50ad718b7c7d7b802a
nonis3 <nonis@purdue.edu>
2017-09-29 20:22:57 -0700

    455A

---
+++ codeforces/0455A_-_Boredom/Main.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static PrintWriter out;
+  public static void main(String[] args) {
+    MyScanner sc = new MyScanner();
+    out = new PrintWriter(new BufferedOutputStream(System.out));
+    int n = sc.nextInt();
+    int MAX = (int)1e6 + 1;
+    long[] c = new long[MAX];
+    long[] dp = new long[MAX];
+    for (int i = 0; i < n; i++)
+      c[sc.nextInt()] += 1;
+
+    dp[0] = 0;
+    dp[1] = c[1];
+
+    long max = c[1];
+    for (int i = 2; i < MAX; i++) {
+      dp[i] = Math.max(dp[i - 1], dp[i - 2] + (c[i] * (long)i));
+      max = Math.max(dp[i], max);
+    }
+
+    out.println(max);
+
+    out.close();
+  }
+
+
+  public static class MyScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public MyScanner() {
+       br = new BufferedReader(new InputStreamReader(System.in));
+    }
+ 
+    public boolean hasNext() {
+      try {
+        boolean a = br.ready();
+        return a;
+      } catch (IOException e) {
+        return false;
+      }
+    }
+
+    public String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+ 
+    public int nextInt() {
+      return Integer.parseInt(next());
+    }
+ 
+    public long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    public char nextChar() {
+      return next().charAt(0);
+    }
+
+    public String nextLine() {
+      String str = "";
+      try {
+         str = br.readLine();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
