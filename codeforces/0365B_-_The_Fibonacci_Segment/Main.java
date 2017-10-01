fc31d2ab9ffd99097db2f0907caf3279ccd35a2a
nonis3 <nonis@purdue.edu>
2017-09-30 20:29:10 -0700

    365B

---
+++ codeforces/0365B_-_The_Fibonacci_Segment/Main.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static PrintWriter out;
+  public static void main(String[] args) {
+    MyScanner sc = new MyScanner();
+    out = new PrintWriter(new BufferedOutputStream(System.out));
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    int[] dp = new int[n];
+    dp[0] = 1;
+    if (n > 1)
+      dp[1] = 2;
+    int m = Integer.MIN_VALUE;
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+      if (i > 1)
+        if (arr[i] == arr[i-1] + arr[i-2])
+          dp[i] = dp[i-1] + 1;
+        else
+          dp[i] = 2;
+      m = Math.max(m, dp[i]);
+    }
+    out.println(m);
+    out.close();
+  }
+
+
+  public static class MyScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public MyScanner() {
+       br = new BufferedReader(new InputStreamReader(System.in));
+    }
+ 
+    public boolean hasNext() {
+      try {
+        boolean a = br.ready();
+        return a;
+      } catch (IOException e) {
+        return false;
+      }
+    }
+
+    public String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+ 
+    public int nextInt() {
+      return Integer.parseInt(next());
+    }
+ 
+    public long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    public char nextChar() {
+      return next().charAt(0);
+    }
+
+    public String nextLine() {
+      String str = "";
+      try {
+         str = br.readLine();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
