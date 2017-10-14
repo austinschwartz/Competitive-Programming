9d9dc135383763b871691a87894b0005fc3bf05e
nonis3 <nonis@purdue.edu>
2017-10-13 21:24:11 -0700

    10551

---
+++ UVA/10551_-_Basic_Remains/Main.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Main {
+  public static PrintWriter out;
+  public static void main(String[] args) {
+    MyScanner sc = new MyScanner();
+    out = new PrintWriter(new BufferedOutputStream(System.out));
+    while (true) {
+      String line = sc.nextLine();
+      if (line.equals("0"))
+        break;
+      String[] sp = line.split(" ");
+      int b = Integer.parseInt(sp[0]);
+      BigInteger p = new BigInteger(sp[1], b);
+      BigInteger m = new BigInteger(sp[2], b);
+      out.println(p.mod(m).toString(b));
+    }
+    out.close();
+  }
+
+
+  public static class MyScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public MyScanner() {
+       br = new BufferedReader(new InputStreamReader(System.in));
+    }
+ 
+    public boolean hasNext() {
+      try {
+        boolean a = br.ready();
+        return a;
+      } catch (IOException e) {
+        return false;
+      }
+    }
+
+    public String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+ 
+    public int nextInt() {
+      return Integer.parseInt(next());
+    }
+ 
+    public long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    public char nextChar() {
+      return next().charAt(0);
+    }
+
+    public String nextLine() {
+      String str = "";
+      try {
+         str = br.readLine();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
