c83b504c02866795e8aa5e15b495fb2349f239a2
nonis3 <nonis@purdue.edu>
2017-10-20 08:09:16 -0700

    272C

---
+++ codeforces/0272C_-_Dima_and_Staircase/main.py
@@ -0,0 +1,15 @@
+import sys
+
+def main():
+    n = int(sys.stdin.readline())
+    a = list(map(int, sys.stdin.readline().split(' ')))
+    m = int(sys.stdin.readline())
+    (w1, h1) = (0, 0)
+    H = 0
+    for i in range(m):
+        (w2, h2) = map(int, sys.stdin.readline().split(' '))
+        H = max(H + h1, a[w2 - 1])
+        sys.stdout.write('{}\n'.format(str(H)))
+        (w1, h1) = (w2, h2)
+
+main()
