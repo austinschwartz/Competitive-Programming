8a013f1712ffbc320078c0686282a15ebd2b89a2
nonis3 <nonis@purdue.edu>
2017-10-26 17:56:20 -0700

    updating c++ template

--- UVA/main.cpp
+++ UVA/main.cpp
@@ -1,22 +1,10 @@
-using namespace std;
 #include <bits/stdc++.h>
-#define inf (1<<30)
-#define eps 1e-9
-typedef long long i64;
-typedef unsigned long long ui64;
-typedef string st;
-typedef vector<int> vi;
-typedef vector<st> vs;
-typedef map<int,int> mii;
-typedef map<st,int> msi;
-typedef set<int> si;
-typedef set<st> ss;
-typedef pair<int,int> pii;
-typedef vector<pii> vpii;
-#define mx 0
 
-int main()
-{
+using namespace std;
+
+#define ll long long
+
+int main() {
   ios_base::sync_with_stdio(0);
   return 0;
 }
