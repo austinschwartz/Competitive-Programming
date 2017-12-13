f1e064fc4c980bb7642aaf0a0d8aee0e6b51b14e
nonis3 <nonis@purdue.edu>
2017-12-13 08:32:27 -0800

    397A

---
+++ codeforces/0397A_-_On_Segments_Own_Points/main.py
@@ -0,0 +1,19 @@
+def main():
+    from collections import defaultdict
+    n = int(input())
+    s = defaultdict(int)
+    (aa, ab) = map(int, input().split(' '))
+    l = []
+    for i in range(n - 1):
+        (a, b) = map(int, input().split(' '))
+        l.append((a, b))
+        while a < b:
+            s[a] += 1
+            a += 1
+    r = 0
+    while aa < ab:
+        if s[aa] == 0:
+            r += 1
+        aa += 1
+    return r
+print(main())
