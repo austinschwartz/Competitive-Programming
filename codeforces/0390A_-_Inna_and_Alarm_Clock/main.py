e1cfc9cc8c45f437268645aea916d62986d5868a
nonis3 <nonis@purdue.edu>
2017-12-13 08:12:56 -0800

    390A

---
+++ codeforces/0390A_-_Inna_and_Alarm_Clock/main.py
@@ -0,0 +1,11 @@
+def main():
+    n = int(input())
+    r = set()
+    c = set()
+    for i in range(n):
+        (x, y) = map(int, input().split(' '))
+        r.add(y)
+        c.add(x)
+    return min(len(r), len(c))
+
+print(main())
